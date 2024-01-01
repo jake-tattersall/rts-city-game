d224f4d8f94e4ad109c626a2f9e711eb88fdac34
Jake Tattersall <147263678+Jakaroo22@users.noreply.github.com>
2024-01-01 03:32:04 -0500

    .

--- main.py
+++ main.py
@@ -1,24 +1,45 @@
 import pygame
-from objects import Test
+from objects import Test, Tower, Unit
 from constants import *
 
+
+def inside(mouse : tuple, rect : pygame.rect.Rect):
+    """Returns true if the mouse is inside of the given rectangle"""
+    if mouse[0] >= rect.x and mouse[0] <= rect.x + rect.width:
+        if mouse[1] >= rect.y and mouse[1] <= rect.y + rect.height:
+            return True
+    return False
+
+
 pygame.init()
-
-
 clock = pygame.time.Clock()
 
 
 win = pygame.display.set_mode(size=(WIDTH, HEIGHT))
 
 pixel = pygame.rect.Rect((10,10), (5,5))
-pixel = Test(10, PLAYER1, win, pixel, velocity=[100,20])
+pixel = Test(1, PLAYER1, win, pixel, velocity=[100,20])
+tower1 = pygame.rect.Rect((300, 500), (TOWER_WIDTH, TOWER_HEIGHT))
+tower1 = Tower(100, PLAYER2, win, tower1)
+unit1 = pygame.rect.Rect((20, 20), (UNIT_WIDTH, UNIT_HEIGHT))
+unit1 = Unit(10, PLAYER1, win, unit1, tower1)
+
+objects = [tower1, unit1]
 
 run = True
 while run:
+
+    pos = pygame.mouse.get_pos()
+
     win.fill((0,0,0))
 
-    pixel.tick()
+    for x in objects:
+        x.tick(objects)
+    #pixel.tick()
 
+    for x in objects:
+        if isinstance(x, Unit) and x.hp == 0:
+            objects.remove(x)
 
     pygame.display.flip()
 
@@ -28,4 +49,4 @@ while run:
         if event.type == pygame.QUIT: 
             run = False
         
-    clock.tick(30)
+    clock.tick(FPS)
--- objects.py
+++ objects.py
@@ -4,6 +4,11 @@ from abc import ABC, abstractmethod
 
 from constants import *
 
+pygame.init()
+towerFont = pygame.font.SysFont('Arial', 20)
+unitFont = pygame.font.SysFont('Arial', 10)
+
+
 class Object(ABC):
     """Parent class for all pieces"""
 
@@ -14,7 +19,7 @@ class Object(ABC):
 
 
     @abstractmethod
-    def tick(self):
+    def tick(self, items : list):
         """"""
 
 
@@ -26,20 +31,21 @@ class Object(ABC):
 class Test(Object):
     """"""
 
-    def __init__(self, hp=0, owner=NEUTRAL, win=None, rect=None, velocity=[0,0]):
+    def __init__(self, hp=0, owner=NEUTRAL, win=None, rect=None, velocity=[0,0], target=None):
         self.hp : int = hp
         self.owner : str = owner
         self.win : pygame.surface.Surface = win
         self.rect : pygame.rect.Rect = rect
         self.velocity : list = velocity
+        self.target : pygame.rect.Rect = target
 
 
-    def tick(self):
+    def tick(self, items : list):
         """"""
         mag = (self.velocity[0] ** 2 + self.velocity[1] ** 2) ** .5
         if mag != 0 and mag != 5:
-            self.velocity[0] *= (5/mag)
-            self.velocity[1] *= (5/mag)
+            self.velocity[0] *= (4/mag)
+            self.velocity[1] *= (4/mag)
 
         self.rect.x += self.velocity[0]
         self.rect.y += self.velocity[1]
@@ -49,6 +55,10 @@ class Test(Object):
 
         if self.rect.y <= 0 or self.rect.y + self.rect.height >= HEIGHT:
             self.velocity[1] = -self.velocity[1]
+
+        for x in items:
+            if isinstance(x, Tower) and self.rect.colliderect(x):
+                x.damage(self)
 
         self.draw()
 
@@ -63,41 +73,97 @@ class Test(Object):
             else:
                 pygame.draw.rect(self.win, GREY, self.rect)
 
-
-
     
 class Tower(Object):
     """"""
 
     def __init__(self, hp=0, owner=NEUTRAL, win=None, rect=None):
-        self.hp = hp
-        self.owner = owner
-        self.win = win
-        self.rect = rect
+        self.hp : int = hp
+        self.owner : str = owner
+        self.win : pygame.surface.Surface = win
+        self.rect : pygame.rect.Rect = rect
+        self.ticks : int = 0
 
 
-    def tick(self):
+    def tick(self, items : list):
         """"""
+        self.ticks += 1
+        if self.ticks == FPS * 2:
+            self.hp += 1
+            self.ticks = 0
+
+        self.draw()
 
 
     def draw(self):
         """"""
+        if self.owner == PLAYER1:
+            pygame.draw.rect(self.win, BLUE, self.rect)
+        elif self.owner == PLAYER2:
+            pygame.draw.rect(self.win, RED, self.rect)
+        else:
+            pygame.draw.rect(self.win, GREY, self.rect)
+
+        text = towerFont.render(str(self.hp), True, WHITE)
+        text_rect = text.get_rect()
+        text_rect.center = self.rect.center
+        self.win.blit(text, text_rect)
+        
+
+    
+    def damage(self, obj : Object):
+        """Take damage. If loses all hp, change sides"""
+        self.hp -= obj.hp
+        if self.hp <= 0:
+            self.owner = obj.owner
+            if self.hp < 0:
+                self.hp = -self.hp
 
 
 
 class Unit(Object):
     """"""
 
-    def __init__(self, hp=0, owner=None, win=None, rect=None):
-        self.hp = hp
-        self.owner = owner
-        self.win = win
-        self.rect = rect
+    def __init__(self, hp=0, owner=NEUTRAL, win=None, rect=None, target=None):
+        self.hp : int = hp
+        self.owner : str = owner
+        self.win : pygame.surface.Surface = win
+        self.rect : pygame.rect.Rect = rect
+        self.target : Tower = target
 
 
-    def tick(self):
+    def tick(self, items : list):
         """"""
+        velocity = [self.target.rect.centerx - self.rect.centerx, self.target.rect.centery - self.rect.centery]
+
+        mag = (velocity[0] ** 2 + velocity[1] ** 2) ** .5
+        if mag != 0 and mag != 5:
+            velocity[0] *= (4/mag)
+            velocity[1] *= (4/mag)
+
+        self.rect.x += velocity[0]
+        self.rect.y += velocity[1]
+
+        for x in items:
+            if isinstance(x, Tower) and self.rect.colliderect(x):
+                x.damage(self)
+                self.hp = 0
+
+        self.draw()
 
 
     def draw(self):
         """"""
+        if self.hp > 0:
+            if self.owner == PLAYER1:
+                pygame.draw.rect(self.win, BLUE, self.rect)
+            elif self.owner == PLAYER2:
+                pygame.draw.rect(self.win, RED, self.rect)
+            else:
+                pygame.draw.rect(self.win, GREY, self.rect)
+
+            text = unitFont.render(str(self.hp), True, WHITE)
+            text_rect = text.get_rect()
+            text_rect.center = self.rect.center
+            text_rect.centery += text_rect.height
+            self.win.blit(text, text_rect)
